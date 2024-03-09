4f8543dcd6d7a40c72e9fd642f0ed45574e0d27e
Adil S <forever@nzironeed.ru>
2024-03-09 15:09:35 +0500

    add pagination for Product/Category views

--- task2/product_shop_app/views.py
+++ task2/product_shop_app/views.py
@@ -1,13 +1,16 @@
 from rest_framework.generics import ListAPIView
 from serializers import CategorySerializer, ProductSerializer
+from paginators import DefaultPagination
 from models import Product, Category
 
 
 class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = DefaultPagination
 
 
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = DefaultPagination
