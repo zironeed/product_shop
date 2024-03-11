da1853c9a1bcbbe4c615f038e56f885148b4018a
Adil S <forever@nzironeed.ru>
2024-03-11 16:31:11 +0500

    add slugify in admin panel

--- task2/product_shop_app/admin.py
+++ task2/product_shop_app/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from models import Category, Subcategory, Product
+from .models import Category, Subcategory, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     list_filter = ('name',)
+
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Subcategory)
@@ -14,9 +16,13 @@ class SubcategoryAdmin(admin.ModelAdmin):
     list_filter = ('name', 'category',)
     search_fields = ('name', 'category',)
 
+    prepopulated_fields = {'slug': ('name',)}
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'category', 'subcategory',)
     list_filter = ('name', 'slug', 'category', 'subcategory',)
     search_fields = ('name', 'slug', 'category', 'subcategory',)
+
+    prepopulated_fields = {'slug': ('name',)}
