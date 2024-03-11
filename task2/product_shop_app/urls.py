dbb75c05478282adbee4a347a010a3b95a97fdec
Adil S <forever@nzironeed.ru>
2024-03-11 16:33:35 +0500

    add urls for jwt and cart

--- task2/product_shop_app/urls.py
+++ task2/product_shop_app/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
-from apps import ProductShopAppConfig
-from views import CategoryListView, ProductListView
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .apps import ProductShopAppConfig
+from .views import CategoryListView, ProductListView, CartViewSet
 
 app_name = ProductShopAppConfig.name
+router = routers.DefaultRouter()
+router.register(f'cart', CartViewSet)
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view, name='cat_list'),
-    path('products/', ProductListView.as_view, name='prod_list'),
+    path('categories/', CategoryListView.as_view(), name='cat_list'),
+    path('products/', ProductListView.as_view(), name='prod_list'),
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
