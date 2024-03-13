290075a9ed6fad1e96bc10b406da7dbd8738e341
Adil S <forever@nzironeed.ru>
2024-03-13 21:29:16 +0500

    update cart models

--- task2/product_shop_app/models.py
+++ task2/product_shop_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 
 
 class Category(models.Model):
@@ -51,15 +50,10 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
-    owner = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='cart_owner')
-    products = models.ManyToManyField('Product', through='CartItem')
-
-    def __str__(self): return {self.owner.name}
+    owner = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='cart')
 
 
 class CartItem(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     quantity = models.PositiveIntegerField(default=1)
-
-    def __str__(self): return f'{self.product.name} - {self.quantity}'
