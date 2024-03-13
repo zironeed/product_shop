7c98465d3ddec8b1ce34e15f1f665b861c20f7fc
Adil S <forever@nzironeed.ru>
2024-03-13 21:32:58 +0500

    add new urls

--- task2/config/urls.py
+++ task2/config/urls.py
@@ -18,9 +18,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Product shop project",
+      default_version='v8',
+      description="Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[AllowAny],
+)
 
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('product_shop_app.urls', namespace='product_shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- task2/product_shop_app/urls.py
+++ task2/product_shop_app/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import ProductShopAppConfig
-from .views import CategoryListView, ProductListView, CartViewSet
+from .views import CategoryListView, ProductListView, CartAPIView, CartItemDeleteAPIView
 
 app_name = ProductShopAppConfig.name
-router = routers.DefaultRouter()
-router.register(f'cart', CartViewSet)
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='cat_list'),
     path('products/', ProductListView.as_view(), name='prod_list'),
-    path('', include(router.urls)),
+
+    path('cart/', CartAPIView.as_view(), name='cart_api'),
+    path('cart/delete/', CartItemDeleteAPIView.as_view(), name='cart_delete'),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
